de187c5e1ee6713e75cdc41633617970bf138449
faelrzin <68194596+faelrzin@users.noreply.github.com>
2026-08-11 19:27:44 -0300

    Add ex02.py with personal information prints

---
+++ Semana01/ex02.py
@@ -0,0 +1,21 @@
+print("hello world")
+idade = "21"
+nome = "rafael oliveira"
+cpf = "000.000.000-00"
+peso = "62 kg"
+data_nascimento = "00/00/2005"
+cidade = "são sepé"
+estado = "rs"
+velocidade = "100 km/h"
+preco = "1000,00"
+
+
+print("idade:", idade)
+print("nome:", nome)
+print("cpf:", cpf)   
+print("peso:", peso)
+print("data de nascimento:", data_nascimento)
+print("cidade:", cidade)
+print("estado:", estado)
+print("velocidade:", velocidade)
+print("preco:", preco)
