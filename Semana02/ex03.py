0ee3250d9865d4035e7883ef08588675ca933529
faelrzin <68194596+faelrzin@users.noreply.github.com>
2026-08-11 21:34:00 -0300

    Add script to calculate double and half of a value

---
+++ Semana02/ex03.py
@@ -0,0 +1,6 @@
+
+valor = float(input("Digite o valor: "))
+valor_final_dobro = valor * 2
+valor_final_metade = valor / 2
+print("Valor final (dobro): ", valor_final_dobro)
+print("Valor final (metade): ", valor_final_metade)
